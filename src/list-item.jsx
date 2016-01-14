4401ed42a6574e3111770ec1a5e0cf7d5d050837
asngai <andrewngai.ucsb@gmail.com>
2016-01-13 17:06:48 -0800

    add to cart button functionality working for firebase side

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -3,18 +3,27 @@ var React = require('react');
 var rootUrl = 'https://shoppingcart-react.firebaseio.com/';
 
 module.exports = React.createClass({
-    //getInitialState: function(){
-    //    return {
-    //        productName: this.props.item.productName,
-    //        productDescription: this.props.item.productDescription,
-    //        price: this.props.item.price,
-    //        inStock: this.props.item.inStock
-    //    }
-    //},
-    //componentWillMount: function(){
-    //    //set each component to point to the corresponding Firebase object
-    //    this.fb = new Firebase(rootUrl + 'catalogue/' + this.props.item.key);
-    //},
+    getInitialState: function(){
+        return {
+            productName: this.props.item.productName,
+            productDescription: this.props.item.productDescription,
+            price: this.props.item.price,
+            inStock: this.props.item.inStock
+        }
+    },
+    componentWillMount: function(){
+        //set each component to point to the corresponding Firebase object
+        ref = new Firebase(rootUrl + 'cart/');
+    },
+    handleAddClick: function(){
+        console.log('here');
+        ref.push({
+            productName: this.props.item.productName,
+            productDescription: this.props.item.productDescription,
+            price: this.props.item.price,
+            inStock: this.props.item.inStock
+        })
+    },
     render: function(){
         return <div className="col-sm-3 item_container">
             <img className="product_img" src="images/mask.jpg" />
@@ -23,7 +32,11 @@ module.exports = React.createClass({
                 <p>{this.props.item.productDescription}</p>
                 <p>{this.props.item.price}</p>
                 <p>{this.props.item.inStock}</p>
-                <button className="btn btn-default">Add to Cart</button>
+                <button
+                    className="btn btn-default"
+                    onClick={this.handleAddClick}>
+                    Add to Cart
+                </button>
             </div>
         </div>
     }
